3b5bd5327d11fe03e01990f1aadd7a2c97d8ea65
tadat216 <tadat216@gmail.com>
2024-12-13 15:11:06 +0700

    .

--- static/main.js
+++ static/main.js
@@ -1,12 +1,29 @@
-const htmlInputs = document.querySelectorAll('.htmlInput');
+const editors = document.querySelectorAll('.editor');
 const renderOutputs = document.querySelectorAll('.htmlOutput');
 
-htmlInputs.forEach((htmlInput, index) => {
-    htmlInput.addEventListener('input', () => {
-        const htmlContent = htmlInput.value;
+editors.forEach((editorElement, index) => {
+    const editor = CodeMirror(editorElement, {
+        mode: 'xml',
+        lineNumbers: true,
+        theme: 'default',
+        value: '<p>hello <span style="color:red;">aaa</span></p>'
+    });
+
+    editor.on('change', (instance) => {
+        const htmlContent = instance.getValue();
+
+        // Cập nhật nội dung của iframe tương ứng
         const doc = renderOutputs[index].contentDocument || renderOutputs[index].contentWindow.document;
         doc.open();
         doc.write(htmlContent);
         doc.close();
     });
+});
+
+const btnExpandCollapse = document.querySelector('.expand-collapse-btn');
+const contentExpandCollapse = document.querySelector('.content-expand-collapse');
+
+btnExpandCollapse.addEventListener('click', () => {
+    contentExpandCollapse.classList.toggle('hidden');
+    btnExpandCollapse.textContent = contentExpandCollapse.classList.contains('hidden') ? '+' : '-';
 });
